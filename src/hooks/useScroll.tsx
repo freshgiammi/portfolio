37a3d12b5cf329cf81e33eb701c61e4856164e30
Gianmarco Rengucci (freshgiammi) <rengucci.gianmarco@gmail.com>
2022-09-20 20:40:43 +0200

    feat: enable useScroll throttling

--- src/hooks/useScroll.tsx
+++ src/hooks/useScroll.tsx
@@ -1,15 +1,14 @@
 /**
  * useScroll React custom hook
  * Usage:
- *    const { scrollX, scrollY, scrollDirection } = useScroll();
+ *    const { scrollX, scrollY, scrollDirection } = useScroll(throttled?: number);
  */
+import lodash from 'lodash';
 import { useState, useEffect } from 'react';
 
 type ScrollDirectionType = 'up' | 'down' | undefined;
 
-export default function useScroll() {
-  // storing this to get the scroll direction
-  const [lastScrollTop, setLastScrollTop] = useState<number>(0);
+export default function useScroll(throttled?: number) {
   // the vertical direction
   const [scrollY, setScrollY] = useState<number>(0);
   // the horizontal direction
@@ -17,19 +16,24 @@ export default function useScroll() {
   // scroll direction would be either up or down
   const [scrollDirection, setScrollDirection] = useState<ScrollDirectionType>();
 
-  const listener = () => {
-    const bodyOffset = document.body.getBoundingClientRect();
-    setScrollY(-bodyOffset.top);
-    setScrollX(bodyOffset.left);
-    setScrollDirection(lastScrollTop > -bodyOffset.top ? 'down' : 'up');
-    setLastScrollTop(-bodyOffset.top);
-  };
-
   // Wait until document is loaded to get the body offset
   useEffect(() => {
-    window.addEventListener('scroll', listener);
+    function listener() {
+      console.debug('New useScroll event, executed at:', new Date().toLocaleTimeString());
+      const bodyOffset = document.body.getBoundingClientRect();
+      console.debug('Previous Y-axis', scrollY);
+      console.debug('Current Y-axis', -bodyOffset.top);
+      console.debug('Assigned direction:', scrollY > -bodyOffset.top ? 'up' : 'down');
+      setScrollY(-bodyOffset.top);
+      setScrollX(bodyOffset.left);
+      setScrollDirection(scrollY > -bodyOffset.top ? 'up' : 'down');
+    }
+
+    const throttledFn = lodash.throttle(listener, throttled, { leading: false, trailing: true });
+
+    window.addEventListener('scroll', throttled ? throttledFn : listener);
     return () => {
-      window.removeEventListener('scroll', listener);
+      window.removeEventListener('scroll', throttled ? throttledFn : listener);
     };
   });
 

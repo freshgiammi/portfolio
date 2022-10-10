cd26e6020a469430b5f05130760a1cbc8149e94d
Gianmarco Rengucci (freshgiammi) <rengucci.gianmarco@gmail.com>
2022-10-10 20:25:25 +0200

    fix: please font, work

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <link
+          href='https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@100;200;300;400;500;600;700&display=swap'
+          rel='stylesheet'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Mukta:wght@200;300;400;500;600;700;800&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
       <body className='bg-sepia-300 transition-colors duration-500 dark:bg-carbon-900'>
         <Main />
         <NextScript />
